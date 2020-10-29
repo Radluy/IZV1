36d4c836e022fc97aacc3b46d0d27e54e699db0d
Radoslav Elias <radoslave0@gmail.com>
2020-10-29 11:22:13 +0100

    meh

--- get_stat.py
+++ get_stat.py
@@ -1,7 +1,8 @@
-import download 
+import download
 import matplotlib.pyplot as plt
 import numpy as np
 import datetime as dm
+import os, argparse
 #tuple(list[str], list[np.ndarray])
 
 def plot_stat(data_source, fig_location=None, show_figure=False):
@@ -31,7 +32,7 @@ def plot_stat(data_source, fig_location=None, show_figure=False):
         "2019": [],
         "2020": []
     }
-
+    #count accidents by year
     for i in range(indices.__len__()-1):
             years = np.datetime_as_string(data_source[1][3][indices[i]:indices[i+1]], unit='Y')
             years_count["2016"].append(np.count_nonzero(years == "2016"))
@@ -40,21 +41,13 @@ def plot_stat(data_source, fig_location=None, show_figure=False):
             years_count["2019"].append(np.count_nonzero(years == "2019"))
             years_count["2020"].append(np.count_nonzero(years == "2020"))
 
+    #plot
     figure, axes = plt.subplots(nrows=len(years_count.keys()), ncols=1)
-    i, j = 0, 0
-    """ax = fig.add_subplot(211)
-    ax.set_title(key)
-    ax.set_xlabel("Regions")
-    ax.bar(regions, years_count[key], color='C3')  #align='center', width=0.5, bottom=0
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    ax.spines['bottom'].set_position('zero')
-    ax.margins(0.05)"""
     for row, key in zip(axes, years_count.keys()):
         row.bar(regions, years_count[key], color='C3')
         row.set_title(key)
-        row.set_xlabel("Regions")
-        row.set_ylabel("Accidents")
+        #row.set_xlabel("Regions")
+        #row.set_ylabel("Accidents")
         row.spines['top'].set_visible(False)
         row.spines['right'].set_visible(False)
         row.spines['bottom'].set_position('zero')
@@ -63,12 +56,23 @@ def plot_stat(data_source, fig_location=None, show_figure=False):
     #plt.setp(row.get_xticklabels()[5:8], color="white")
     #plt.setp(row.get_xticklines()[10:15], markeredgecolor="white")
     plt.tight_layout()
-    plt.show()
+
+    if (fig_location):
+        if (not os.path.exists(os.path.dirname(fig_location))):
+            os.mkdir(os.path.dirname(fig_location))
+        plt.savefig(fig_location)
+
+    if (show_figure):
+        plt.show()
     print("DONE")
 
 
 
+if (__name__ == "__main__"):
+    parser = argparse.ArgumentParser(description='plot some things')
+    parser.add_argument('--show_figure', default=None, action='store_true', help="show figure")
+    parser.add_argument("--fig_location", dest="fig_location", default=None)
+    args = parser.parse_args()
 
-
-data_source = download.DataDownloader().get_list(["STC", "HKK", "JHM"])
-plot_stat(data_source)
+    data_source = download.DataDownloader().get_list(["STC", "HKK", "JHM"])
+    plot_stat(data_source, args.fig_location, args.show_figure)
